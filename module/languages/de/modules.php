985fbf90f8fd4ba80221a846eeba637b873cd228
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-03-09 17:51:53 +0100

    Translate

---
+++ module/languages/de/modules.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * E-POSTBUSINESS API integration for Contao Open Source CMS
+ *
+ * Copyright (c) 2015-2017 Richard Henkenjohann
+ *
+ * @package E-POST
+ * @author  Richard Henkenjohann <richard-epost@henkenjohann.me>
+ */
+
+$GLOBALS['TL_LANG']['MOD']['newsletter2go_users'][0] = 'Newsletter2Go-Benutzer';
+$GLOBALS['TL_LANG']['MOD']['newsletter2go_users'][1] = 'Verwalten Sie die Nutzer, die Zugriff auf Ihr Newsletter2Go-System haben. Weisen Sie diese API-Benutzer den Backend-Benutzern zu.';
---
+++ module/languages/de/tl_newsletter2go_user.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * FERIENPASS extension for Contao Open Source CMS built on the MetaModels extension
+ *
+ * Copyright (c) 2015-2016 Richard Henkenjohann
+ *
+ * @package Ferienpass
+ * @author  Richard Henkenjohann <richard@ferienpass.online>
+ */
+
+
+$table = Newsletter2Go\ContaoSync\Model\Newsletter2GoUser::getTable();
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG'][$table]['title_legend'] = 'Name und Key';
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG'][$table]['name'][0] = 'Name';
+$GLOBALS['TL_LANG'][$table]['name'][1] = 'Bitte geben Sie einen internen Namen für den API-Benutzer ein.';
+$GLOBALS['TL_LANG'][$table]['authKey'][0] = 'Auth-Key';
+$GLOBALS['TL_LANG'][$table]['authKey'][1] = 'Bitte geben Sie den Auth-Key ein, den Sie im Backend von Newsletter2Go finden.';
+
+/**
+ * Actions
+ */
+$GLOBALS['TL_LANG'][$table]['new'][0] = 'Neuer API-Benutzer';
+$GLOBALS['TL_LANG'][$table]['new'][1] = 'Einen neuen API-Benutzer erstellen';
+$GLOBALS['TL_LANG'][$table]['edit'][0] = 'Bearbeiten';
+$GLOBALS['TL_LANG'][$table]['edit'][1] = 'Den API-Benutzer ID %s bearbeiten';
+$GLOBALS['TL_LANG'][$table]['show'][0] = 'Details zeigen';
+$GLOBALS['TL_LANG'][$table]['show'][1] = 'Details vom API-Benutzer ID %s anzeigen';
+$GLOBALS['TL_LANG'][$table]['authenticate'][0] = 'Authentifizieren';
+$GLOBALS['TL_LANG'][$table]['authenticate'][1] = 'Den API-Benutzer ID %s authentifizieren';
